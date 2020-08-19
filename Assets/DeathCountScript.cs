de456f1317f202735bef322e363c2015f03e25e6
Colby Saenthavy <thedarkness435@gmail.com>
2020-08-19 11:34:35 -0600

    Started on Achievement stuffs, Woo

--- Assets/DeathCountScript.cs
+++ Assets/DeathCountScript.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 
 public class DeathCountScript : MonoBehaviour
 {
-    private int deathCount = 0;
+    private int deathCount;
     public Text deathCountText;
     bool isActivated = false;
 
+
+    /* Updates the deathCount to equal the saved amount of deaths
+     then replaces the canvas text to have the deathcount displayed
+    man if only I could have been able to do the instantiate in 1 line*/
     void Start()
     {
         deathCount = PlayerPrefs.GetInt("DeathCount");
@@ -21,6 +25,9 @@ public class DeathCountScript : MonoBehaviour
         isActivated = true;
     }
 
+    /* This is where it does a check if the current deathcount is larger than the
+     * deathcount that has been saved. its the same way for highscore, but I think
+     this can be placed at better timing (Ya know like when the Cabbit dies), FML*/
     public void Deactivate()
     {
         isActivated = false;
---
+++ Assets/Scripts/Achievements.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* An Enum of Achievements possible to get, I honestly just thought this
+ * would be a cool way to do it.*/
+public enum AchievementEnums
+{
+    DIE = 0,
+    DIETENTIMES = 1,
+    DIETHIRTYTIMES = 2,
+    DIEFIFTYTIMES = 3,
+}
+
+public class Achievements : MonoBehaviour
+{
+    private List<AchievementEnums> AchievementsAchieved;
+
+
+    void Start()
+    {
+        AchievementsAchieved = new List<AchievementEnums>();
+    }
+
+    void checkDeathAchievements()
+    {
+        int deathCount = PlayerPrefs.GetInt("DeathCount");
+
+        /* Just Checks if the current Death is higher than a greater Number
+         * This is honestly a terrible way of doing it. */
+        if(deathCount >= 1)
+        {
+            AchievementsAchieved.Add(AchievementEnums.DIE);
+        }
+        if (deathCount >= 10)
+        {
+            AchievementsAchieved.Add(AchievementEnums.DIETENTIMES);
+        }
+        if (deathCount >= 30)
+        {
+            AchievementsAchieved.Add(AchievementEnums.DIETHIRTYTIMES);
+        }
+        if (deathCount >= 50)
+        {
+            AchievementsAchieved.Add(AchievementEnums.DIEFIFTYTIMES);
+        }
+
+    }
+
+}
