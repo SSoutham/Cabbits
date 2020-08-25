f62a0e544d74769e55018c3ead7da99ebb8bed88
Colby Saenthavy <thedarkness435@gmail.com>
2020-08-25 11:22:30 -0600

    Achievement system working
    will now focus on more specific achievements

--- Assets/Scripts/AchievementsPane.cs
+++ Assets/Scripts/AchievementsPane.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class AchievementsPane : MonoBehaviour
 {
     [SerializeField] Canvas achievementPane;
+    [SerializeField] GameObject die1;
+    [SerializeField] GameObject die10;
+    [SerializeField] GameObject die30;
+    [SerializeField] GameObject die50;
 
     private bool isActive = false;
 
@@ -13,17 +17,47 @@ public class AchievementsPane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /* User Presses Escape, Achievement Pane pops up,
+         * simple as that. */
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             if(isActive == false)
             {
                 achievementPane.enabled = true;
                 isActive = true;
+                CheckAchievements(Achievements.AchievementsAchieved);
+
             }
             else
             {
                 achievementPane.enabled = false;
                 isActive = false;
+            }
+        }
+    }
+
+    void CheckAchievements(List<AchievementEnums> achievements)
+    {
+        if(achievements.Count != 0)
+        {
+            for(int i = 0; i < achievements.Count; i++)
+            {
+                if(achievements[i] == AchievementEnums.DIE)
+                {
+                    die1.SetActive(true);
+                }
+                else if(achievements[i] == AchievementEnums.DIETENTIMES)
+                {
+                    die10.SetActive(true);
+                }
+                else if(achievements[i] == AchievementEnums.DIETHIRTYTIMES)
+                {
+                    die30.SetActive(true);
+                }
+                else
+                {
+                    die50.SetActive(true);
+                }
             }
         }
     }
